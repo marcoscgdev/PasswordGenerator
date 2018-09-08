f1fb4b99c7f2cbfb9c9c14f455e4bc224f29cd93
Marcos Calvo García <marcoscgdev@gmail.com>
2018-09-08 13:24:17 +0200

    Fixed about dialog.

--- app/src/main/java/com/marcoscg/passwordgenerator/MainActivity.java
+++ app/src/main/java/com/marcoscg/passwordgenerator/MainActivity.java
@@ -164,7 +164,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_info:
-                AboutDialog.with(this)
+                new AboutDialog.Builder(this)
                         .setAuthor("@MarcosCGdev")
                         .setWebsite("www.marcoscg.com")
                         .show();
